ffc7f34c9d7f51a3b7a895b08b3e3794ac189cef
John Pham <mr.john.pham@gmail.com>
2020-07-15 13:01:25 -0400

    Serve JSON data

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+$myObj = new \stdClass();
+
 $myObj->name = "John";
 $myObj->age = 30;
 $myObj->city = "New York";
 
 $myJSON = json_encode($myObj);
+
+header('Content-Type: application/json');
 
 echo $myJSON;
 ?>

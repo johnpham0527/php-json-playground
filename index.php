b173f5a8faf501e300165711763f2658154ba73e
John Pham <mr.john.pham@gmail.com>
2020-07-26 05:42:59 -0400

    Add head and body tags

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
-<?php
-$data = new \stdClass();
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP JSON Playground</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <?php
+            $data = new \stdClass();
 
-$file = file_get_contents('./data/qplFY2019Programs.json', true);
+            $file = file_get_contents('./data/qplFY2019Programs.json', true);
 
-// $data->name = "John";
-// $data->age = 30;
-// $data->city = "New York";
+            // $data->name = "John";
+            // $data->age = 30;
+            // $data->city = "New York";
 
-$allData = json_decode($file, true);
+            $allData = json_decode($file, true);
 
-// $month = $allData["Month"];
-// $familySessions = $allData["Family Sessions"];
+            // $month = $allData["Month"];
+            // $familySessions = $allData["Family Sessions"];
 
-header('Content-Type: application/json');
-// echo json_encode($familySessions[0]);
+            header('Content-Type: application/json');
+            // echo json_encode($familySessions[0]);
 
-?>
+            echo $allData[0];
+
+    ?>   
+    </body>
+</html>
+
